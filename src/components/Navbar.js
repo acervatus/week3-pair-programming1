eb7534208727cefe427ccc9dae84e4364659f635
Alvari <alvarip@metropolia.fi>
2024-01-29 16:34:09 +0200

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,50 @@
+import logo from '../images/logo.svg'
+import PageLinks from './PageLinks';
 const Navbar = () => {
   return (
-    <div>
-      <p className="section-dummy">About</p>
-      <p className="section-dummy">...</p>
+    <nav className="navbar">
+    <div className="nav-center">
+      <div className="nav-header">
+      <img src={logo} className='nav-logo' alt='backroads' />
+        <button type="button" className="nav-toggle" id="nav-toggle">
+          <i className="fas fa-bars"></i>
+        </button>
+      </div>
+      <PageLinks parentClass='footer-links' itemClass='footer-link' />
+      <ul className="nav-icons">
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-icon"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-icon"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-icon"
+          >
+            <i className="fab fa-squarespace"></i>
+          </a>
+        </li>
+      </ul>
     </div>
+  </nav>
   );
 };
 
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,9 +1,55 @@
+import Tour from './Tour';
+const tours = [
+  {
+    id: 1,
+    image: 'https://www.lomamatkalle.fi/wp-content/uploads/lomamatkat-vaasasta.jpg',
+    date: '2024-01-01',
+    title: 'Tour 1',
+    location: 'indonesia',
+    duration: '10',
+    cost: '1000'
+  },
+  {
+    id: 2,
+    image: 'https://lomaco.fi/wp-content/uploads/2021/05/sebastian-pena-lambarri-Yaia5KzMI-Y-unsplash-scaled.jpg',
+    date: '2024-01-02',
+    title: 'Tour 2',
+    location: 'turkey',
+    duration: '16',
+    cost: '5000'
+  },
+  {
+    id: 3,
+    image: 'https://media.cntraveler.com/photos/56fd4d7a031da97d43d11079/master/pass/budapest-GettyImages-178060240.jpg',
+    date: '2024-01-03',
+    title: 'Tour 3',
+    location: 'england',
+    duration: '3',
+    cost: '10004'
+  },
+  {
+    id: 4,
+    image: 'https://www.thefivefoottraveler.com/wp-content/uploads/2016/09/IMG_7935.jpg',
+    date: '2024-01-04',
+    title: 'Tour 4',
+    location: 'finland',
+    duration: '6',
+    cost: '1342'
+  },
+  // more tour objects...
+];
 function Tours() {
   return (
-    <div>
-      <p className="section-dummy">Tours</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <section className="section" id="tours">
+          <div className="section-title">
+            <h2>featured <span>tours</span></h2>
+          </div>
+          <div className="section-center featured-center">
+          {tours.map((tour) => (
+                <Tour key={tour.id} {...tour} />
+                ))}
+          </div>
+        </section>
   );
 }
 
